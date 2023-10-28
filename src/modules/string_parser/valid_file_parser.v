6a64b2e485efd83e901c5cb1f80ac7fefe5edf5a
Yannick Fricke <yannickfricke@googlemail.com>
2023-10-28 08:51:31 +0200

    fix(string_parser): fixed the valid_file_parser
    Before it checked if the path was a directory and returned some string if
    this was the case. But we want to check for files - not directories.

--- src/modules/string_parser/valid_file_parser.v
+++ src/modules/string_parser/valid_file_parser.v
@@ -5,7 +5,7 @@ import os
 pub fn valid_file_parser(input string) ?string {
 	absolute_path := os.abs_path(input)
 
-	return if os.is_dir(absolute_path) {
+	return if os.is_dir(absolute_path) == false {
 		absolute_path
 	} else {
 		none
